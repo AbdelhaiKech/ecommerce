3e11f6634a931eef543b88d803d229f7a804e98b
abdelhai <abdelhai.seocom@gmail.com>
2022-09-01 18:27:20 +0100

    cree master page utilisateur

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.masteruser')
 
 @section('content')
 <div class="container">
@@ -9,21 +9,42 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">name : {{$user->name}}</li>
-                        <li class="list-group-item">Telephone : {{$user->phone}}</li>
-                        <li class="list-group-item">Adresse : {{$user->adresse}}</li>
-                        @if({{$user->usertype}} == 0)
-                         <li class="list-group-item">Role : User</li>
-                         @else
-                         <li class="list-group-item">Role : Admin</li>
-                        @endif
-                    </ul>
-                    
+                    <div class="col-md-12">
+                        <div
+                            class="row g-0 border rounded overflow-hidden flex-md-row mb-46 shadow-sm h-md-250 position-relative">
+                            <div class="col p-4 d-flex flex-column position-static">
+                                <strong class="d-inline-block mb-2 text-primary">Profil</strong>
+
+                                <h5 class="mb-0">
+                                    <strong class="d-inline-block mb-2 text-dark">Name : </strong> {{$user->name}}</h5>
+                                <h5 class="mb-0"> <strong class="d-inline-block mb-2 text-dark">Phone : </strong>
+                                    {{$user->phone}}</h5>
+                                <h5 class="mb-0"><strong class="d-inline-block mb-2 text-dark">Adresse : </strong>
+                                    {{$user->adresse}}</h5>
+
+
+                                @if($user->usertype == 0)
+                                <h5><strong class="d-inline-block mb-2 text-dark">Type : </strong><span class="badge bg-success text-light">User</span></h5>
+                                @else
+                                
+                                <h5><strong class="d-inline-block mb-2 text-dark">Type : </strong><span class="badge bg-danger text-light">Admin</span></h5>
+                                @endif
+
+                            </div>
+
+                            <div class="col-auto d-none d-lg-block">
+                                <img class="card-img-left example-card-img-responsive"
+                                    src="https://cdn.pixabay.com/photo/2022/08/26/22/01/bee-7413333_960_720.jpg"
+                                    style="    width: 300px;" />
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/index');
+});
+Route::get('/tm', function () {
+    return view('pages/index');
 });
 
 Auth::routes();
